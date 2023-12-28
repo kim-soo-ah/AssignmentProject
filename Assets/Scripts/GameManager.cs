3cee9762abca7a8ea6d4ca7df1c3874a474cbc79
kim-soo-ah <149459760+kim-soo-ah@users.noreply.github.com>
2023-12-29 03:46:12 +0900

    feat:"UI추가"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class GameManager : SingletonHandler<GameManager>
 {
     public Transform Player {  get; private set; }
     [SerializeField] private string playerTag = "Player";
+    private HealthSystem playerHealthSystem;
 
-    
+    [SerializeField] private TMP_Text waveText;
+    [SerializeField] private Slider hpGaugeSlider;
+    [SerializeField] private GameObject gameOverUI;
 
     protected override void Awake()
     {
         base.Awake();
         Player = GameObject.FindGameObjectWithTag(playerTag).transform;
+
+        playerHealthSystem = Player.GetComponent<HealthSystem>();
+        playerHealthSystem.OnDamage += UpdateHealthUI;
+        playerHealthSystem.OnHeal += UpdateHealthUI;
+        playerHealthSystem.OnDeath += GameOver;
+
+        gameObject.SetActive(false);
+    }
+
+    private void GameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+    private void UpdateHealthUI()
+    {
+        hpGaugeSlider.value = playerHealthSystem.CurrentHealth / playerHealthSystem.MaxHealth;
+    }
+
+    private void UpdateWaveUI()
+    {
+        //waveText.text
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
